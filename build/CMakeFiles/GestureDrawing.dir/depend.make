5a7e2f667a71e851ae9319df2bab0c4485c83ad9
jonathanzhu <jiasunzhu@gmail.com>
2025-06-04 23:59:15 -0400

    chore: restructuring

--- build/CMakeFiles/GestureDrawing.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GestureDrawing.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jonathanzhu/clionprojects/GestureDrawing
-# Build directory: /Users/jonathanzhu/clionprojects/GestureDrawing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
